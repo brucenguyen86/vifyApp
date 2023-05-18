da3ae2f08dffccf9f949e7b608d24f1a3f6350f0
brucenguyen86 <130854166+brucenguyen86@users.noreply.github.com>
2023-05-18 10:17:57 +0700

    changed index.jsx

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -13,6 +13,7 @@ import { TitleBar } from "@shopify/app-bridge-react";
 import { trophyImage } from "../assets";
 
 import { ProductsCard } from "../components";
+import React from "react";
 
 export default function HomePage() {
     return (
@@ -29,52 +30,11 @@ export default function HomePage() {
                         >
                             <Stack.Item fill>
                                 <TextContainer spacing="loose">
-                                    <Text as="h2" variant="headingMd">
-                                        This is my App 🎉
-                                    </Text>
-                                    <p>
-                                        Your app is ready to explore! It contains everything you
-                                        need to get started including the{" "}
-                                        <Link url="https://polaris.shopify.com/" external>
-                                            Polaris of Bruce
-                                        </Link>
-                                        ,{" "}
-                                        <Link url="https://shopify.dev/api/admin-graphql" external>
-                                            Shopify Admin API
-                                        </Link>
-                                        , and{" "}
-                                        <Link
-                                            url="https://shopify.dev/apps/tools/app-bridge"
-                                            external
-                                        >
-                                            App Bridge
-                                        </Link>{" "}
-                                        UI library and components.
-                                    </p>
-                                    <p>
-                                        Ready to go? Start populating your app with some sample
-                                        products to view and test in your store.{" "}
-                                    </p>
-                                    <p>
-                                        Learn more about building out your app in{" "}
-                                        <Link
-                                            url="https://shopify.dev/apps/getting-started/add-functionality"
-                                            external
-                                        >
-                                            this Shopify tutorial
-                                        </Link>{" "}
-                                        📚{" "}
-                                    </p>
+                                   I would like to write something here
                                 </TextContainer>
                             </Stack.Item>
                             <Stack.Item>
-                                <div style={{ padding: "0 20px" }}>
-                                    <Image
-                                        source={trophyImage}
-                                        alt="Nice work on building a Shopify app"
-                                        width={120}
-                                    />
-                                </div>
+                                what is a stack Item?
                             </Stack.Item>
                         </Stack>
                     </Card>
